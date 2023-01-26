da55d01fbd95e84c98b577feafb94b3d3f75896c
Andreas Ekberg <ekbergarnyt@gmail.com>
2023-01-26 16:50:17 +0100

    More clean code

--- script.js
+++ script.js
@@ -4,19 +4,73 @@ console.log("Connected");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-//Funktion för att rita en boll som tar in 4 variabler
-// x-kord, y-kord, radius, färg
-function drawBall(x, y, r, color) {
-  ctx.beginPath();
-  //Ritar en cirkel
-  ctx.arc(x, y, r, 0, 2 * Math.PI);
-  //Fyller den med färg
-  ctx.fillStyle = color;
-  //Kanten på bollen
-  ctx.stroke();
-  //fyller den
-  ctx.fill();
+//Bools för att se ifall knappen är nertryckt
+let UP;
+
+//Array med alla bollar
+let ballList = [];
+
+class Ball {
+  constructor(x, y, r, color) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.color = color;
+    ballList.push(this);
+  }
+
+  //Funktion för att rita en boll som tar in 4 variabler
+  // x-kord, y-kord, radius, färg
+  drawBall() {
+    ctx.beginPath();
+    //Ritar en cirkel
+    ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+    //Fyller den med färg
+    ctx.fillStyle = this.color;
+    //Kanten på bollen
+    ctx.stroke();
+    //fyller den
+    ctx.fill();
+  }
 }
 
-drawBall(160, 380, 20, "white");
-drawBall(160, 180, 20, "red");
+//Event handler för att lyssna på när man trycker ner en knapp
+canvas.addEventListener("keydown", function (e) {
+  //Lyssna och hitta vad key har för kod
+  //console.log(e);
+
+  if (e.keyCode === 38) {
+    UP = true;
+  }
+});
+
+//Event handler för att lyssna ifall man släpper en knapp
+canvas.addEventListener("keyup", function (e) {
+  if (e.keyCode === 38) {
+    UP = false;
+  }
+});
+
+//Funktion för rörelse
+function move() {
+  if (UP) {
+    mainBall.y--;
+  }
+}
+
+//Animerar canvas varje frame
+function mainLoop() {
+  requestAnimationFrame(mainLoop);
+  //clear canvas
+  ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+  move();
+  ballList.forEach((b) => {
+    b.drawBall();
+  });
+}
+
+//Definerar bollarna
+let mainBall = new Ball(160, 380, 20, "white");
+let Ball1 = new Ball(160, 180, 20, "red");
+
+requestAnimationFrame(mainLoop);
